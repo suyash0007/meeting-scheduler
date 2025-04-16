475cac0a4c3a39de29c2fe210c94f252751d33e1
Suyash Tiwari <tiwarisuyash40@gmail.com>
2025-04-16 13:11:08 +0530

    Update authSlice.ts

--- src/redux/features/authSlice.ts
+++ src/redux/features/authSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface User {
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+  id?: string | null;
+}
+
 interface AuthState {
   isAuthenticated: boolean;
-  user: {
-    name?: string;
-    email?: string;
-    image?: string;
-  } | null;
+  user: User | null;
 }
 
 const initialState: AuthState = {
@@ -18,7 +21,7 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<any>) => {
+    setUser: (state, action: PayloadAction<User>) => {
       state.isAuthenticated = true;
       state.user = action.payload;
     },
